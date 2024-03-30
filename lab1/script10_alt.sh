5bca19fcb599833b9f1727149428dbf248aa4443
Croshaw <crosafixx200215@gmail.com>
2024-03-30 08:40:50 +0500

    fix

--- lab1/script10_alt.sh
+++ lab1/script10_alt.sh
@@ -1,1 +1,1 @@
-man bash | tr ' ' '\n' | tr '[:punct:]' '\n' | tr '[:upper:]' '[:lower:]' | awk 'length($0) >= 4' | sort | uniq -c | sort -nr | head -n 3
+man bash | tr '[:punct:]' ' ' | tr '[:space:]' '\n' | tr '[:upper:]' '[:lower:]' | awk 'length($0) >= 4' | sort | uniq -c | sort -nr | head -n 3
