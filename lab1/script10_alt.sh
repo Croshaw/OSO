9dcec15208b98db7f81874ab9a2d19ef175d58dc
Croshaw <crosafixx200215@gmail.com>
2024-03-30 08:34:58 +0500

    fix alt 10 script

--- lab1/script10_alt.sh
+++ lab1/script10_alt.sh
@@ -1,1 +1,1 @@
-man bash | tr '[:punct:]' '\n' | tr '[:upper:]' '[:lower:]' | grep -oE '\b\w{4,}\b' | sort | uniq -c | sort -nr | head -n 3
+man bash | tr ' ' '\n' | tr '[:punct:]' '\n' | tr '[:upper:]' '[:lower:]' | awk 'length($0) >= 4' | sort | uniq -c | sort -nr | head -n 3
