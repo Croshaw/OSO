75c324df81507ceab5ac1eaca34a1182819d7bec
Croshaw <crosafixx200215@gmail.com>
2024-04-26 23:15:01 +0500

    fix script7 in lab2

--- lab2/script7.sh
+++ lab2/script7.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
-declare -a read_bytes
+declare -A read_bytes
 
 for dir in /proc/[0-9]*; do
 	pid=$(basename $dir)
-	read=$(awk '/read_bytes:/{print $2}' "$dir/io")
-	read_bytes[$pid]=read	
+	read_b=$(awk '/read_bytes:/{print $2}' "$dir/io")
+	read_bytes[$pid]=$read_b	
 done
-sleep 10
+sleep 60
 for dir in /proc/[0-9]*; do
 	pid=$(basename $dir)
-	read=$(awk '/read_bytes:/{print $2}' "$dir/io")
+	read_b=$(awk '/read_bytes:/{print $2}' "$dir/io")
 	if [[ -n "read_bytes[$pid]" ]]; then
-		read_bytes[$pid]=$(echo "$read - $read_bytes[$pid]" | bc)
+		read_bytes[$pid]=$(echo "$read_b - ${read_bytes[$pid]}" | bc)
 	else
-		read_bytes[$pid]=$read
+		read_bytes[$pid]=$read_b
 	fi
 done
-IFS=$'\n' sorted=($(sort -n <<<"${even[*]}"))
+#IFS=$'\n' sorted=($(sort -n <<<"${even[*]}"))
+#unset IFS
 
-for value in "${sorted[@]}"; do
-	echo "$value"
+echo "$(for key in "${!read_bytes[@]}"; do
+	echo "$key:${read_bytes[$key]}"
+done | sort -n -r -k 2 | head -n 3)" | while IFS=":" read -r pid read_b; do
+	if [[ -f "/proc/$pid/cmdline" ]]; then
+		cmdline=$(tr -d '\0' < "/proc/$pid/cmdline")
+	fi
+	if [[ ! -n "$cmdline" ]]; then
+		cmdline="none"
+	fi
+	echo "$pid:$cmdline:$read_b"
 done
