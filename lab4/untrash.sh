b83cd981b2d349c06cd235cfb94d8e4eb9119858
Croshaw <crosafixx200215@gmail.com>
2024-06-03 22:06:54 +0500

    yra

--- lab4/untrash.sh
+++ lab4/untrash.sh
@@ -8,13 +8,12 @@ if [[ ! -f "$HOME/.trash.log" ]]; then
 	echo 'File ne nayden';
 	exit 0;
 fi
-while IFS=' : ' read -r path name || [[ -n $line ]]; do
-	echo "$(basename $path)"
+while IFS=' : ' read -r path name; do
+	#echo "$(basename $path)"
 	if [[ "$(basename $path)" == "$1" ]]; then
 		echo "Xotite vostanovit $path? (y=yes,n=no)";
 		while true; do
-			read -r input;
-			echo "$input 1";
+			read -r input <&1;
 			if [[ "$input" == "yes" || "$input" == "y" ]]; then
 				echo "$(dirname "$path")";
 				if [[ ! -e $(dirname "$path") ]]; then
@@ -26,17 +25,25 @@ while IFS=' : ' read -r path name || [[ -n $line ]]; do
 						break;
 					fi
 					echo "Konflict filov, vvedite novoe Im9 fila: ";
-					read newFName;
-					$path="$(basename $path)/$newFName";
+					read -r newFName <&1;
+					#echo "$(dirname $path)";
+					path="$(dirname $path)/$newFName";
+					#echo "$path";
 				done
 				ln -P "$HOME/.trash/$name" "$path";
 				rm "$HOME/.trash/$name";
 				break;
 			fi
 			if [[ "$input" == "no" || "$input" == "n" ]]; then
+				echo "$path : $name" >> "$HOME/.temp.rem";
 				break;
 			fi
-		done	
-
+		done
+	else
+		echo "$path : $name" >> "$HOME/.temp.rem";
 	fi
-done < "$HOME/.trash.log"
+done < "$HOME/.trash.log";
+if [[ -f "$HOME/.temp.rem" ]]; then
+	cat "$HOME/.temp.rem" > "$HOME/.trash.log";
+	rm "$HOME/.temp.rem";
+fi
